8ed5f0539d26fb05ce8787cf91e0bef28dfe8913
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-09-17 14:02:09 -0400

    Slightly revised ttr.py

--- ttr/ttr.py
+++ ttr/ttr.py
@@ -4,30 +4,33 @@ import sys
 
 
 def main():
-	filename = sys.argv[1]
-	delimiter = sys.argv[2]
-	strip_punctuation = sys.argv[3]
+    filename, delimiter, strip_punctuation = sys.argv[1:]
 
-	word_count = 0
-	word_set = set()
-	regex = re.compile('[%s]' % re.escape(string.punctuation + strip_punctuation))
+    word_count = 0
+    word_set = set()
+    strip_punctuation = re.compile('[%s]' % re.escape(strip_punctuation +
+                                                      string.punctuation +
+                                                      '\n'))
 
-	with open(filename, 'r') as file:	
-		for line in file:
-			line = regex.sub('', line.lower())
-			words = line.strip().split(delimiter)
-			for w in words:
-				if w != '':
-					word_set.add(w)
-					word_count += 1
+    with open(filename, 'r') as file:
+        for line in file:
+            line = strip_punctuation.sub('', line.lower())
+            #words = line.strip().split(delimiter)
+            words = line.split(delimiter)
+            for w in words:
+                if w != '':
+                    word_set.add(w)
+                    word_count += 1
 
-	distinct_word_count = len(word_set)
-	ttr = distinct_word_count / word_count
-	print ("distinct_word_count", distinct_word_count)
-	print ("word_count", word_count)
-	print ("ttr", ttr)
-	#print (word_set)
+    distinct_word_count = len(word_set)
+    ttr = distinct_word_count / word_count
+
+    print ('distinct_word_count =', distinct_word_count)
+    print ('word_count =', word_count)
+    print ('ttr =', ttr)
+
+    print ('word_set =', word_set)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
