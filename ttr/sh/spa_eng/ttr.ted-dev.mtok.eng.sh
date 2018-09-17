43a22cc9150db7bb5899a52de9a5a2cb0a9f51c9
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-09-17 16:33:12 -0400

    .

--- ttr/sh/spa_eng/ttr.ted-dev.mtok.eng.sh
+++ ttr/sh/spa_eng/ttr.ted-dev.mtok.eng.sh
@@ -1,1 +1,1 @@
-python ttr.py /home/gneubig/exp/transfer-exp/data/spa_eng/ted-dev.mtok.eng " " "\\b&quot;\\b" > ~/ttr_result/spa_eng/ted-dev.mtok.eng.ttr
+python /home/yuhsianl/phoneme/ttr/ttr.py /home/gneubig/exp/transfer-exp/data/spa_eng/ted-dev.mtok.eng " " "\\b&quot;\\b" > /home/yuhsianl/ttr_result/spa_eng/ted-dev.mtok.eng.ttr
