fcb6f9f4adfb8272deed2fe17f39e98806f068dd
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-12-06 14:21:40 -0500

    Revise GenerateDataFile_RankMT.py

--- ranking/GenerateDataFile_RankMT.py
+++ ranking/GenerateDataFile_RankMT.py
@@ -13,10 +13,11 @@ from collections import defaultdict
 if __name__ == "__main__":
     # Working directory on clio
     root = "/home/yuhsianl/public/phoneme_common_data/data/mt"
+    # URIEL distance
+    distance_root = "/home/yuhsianl/public/phoneme_common_data/data/uriel_v0_2/distances"
 
     # Working directory on your local machine
-    # root = "/Users/yuhsianglin/Dropbox/cmu/11634A_11632A_capstone/20181029 Jupyter notebook"
-
+    # root = "/Users/yuhsianglin/Dropbox/cmu/phoneme_data/mt"
 
     ### Load baseline experiment result: Get ranking ###
     # mt.csv is Prof. Neubig's MT experiment results
@@ -31,23 +32,23 @@ if __name__ == "__main__":
         for lan in lang_set:
             print(lan, file=f)
 
-    table = [["Task lang", "Aux lang", "BLEU"]]
+    table = [["Target lang", "Transfer lang", "BLEU"]]
     rank = ["Rank"]
     BLEU_level = ["BLEU level"]
 
-    for lan_task in lang_set:
+    for lan_tg in lang_set:
         rank_score = []
-        for lan_aux in lang_set:
-            if lan_task == lan_aux or baseline.loc[lan_aux, lan_task] == 'X':
+        for lan_tf in lang_set:
+            if lan_tg == lan_tf or baseline.loc[lan_tf, lan_tg] == 'X':
                 continue
-            table.append([str(lan_task), str(lan_aux), float(baseline.loc[lan_aux, lan_task])])
+            table.append([str(lan_tg), str(lan_tf), float(baseline.loc[lan_tf, lan_tg])])
             # Note: use negative BLEU score here
             # scipy.stats.rankdata() gives rank [1, 2, 3, ...] to negative BLEU scores [-0.3, -0.2, -0.1],
             # so higher BLEU score is put in the front (small "rank" integer value) of the ranking result
-            rank_score.append(-float(baseline.loc[lan_aux, lan_task]))
+            rank_score.append(-float(baseline.loc[lan_tf, lan_tg]))
 
         rank.extend(rankdata(rank_score, 'min'))
-        BLEU_level.extend(rankdata(list(-np.array(rank_score)), 'min'))
+        BLEU_level.extend(rankdata(list(-np.array(rank_score)), 'max'))
 
     table = np.column_stack((np.array(table), np.array(rank), np.array(BLEU_level)))
 
@@ -69,7 +70,6 @@ if __name__ == "__main__":
     datasize_table = datasize_table.set_index("Language")
 
     # URIEL distance
-    distance_root = "/home/yuhsianl/public/phoneme_common_data/data/uriel_v0_2/distances"
     distance_category = [fname.split(".")[0] for fname in os.listdir(distance_root) if fname.split(".")[1] == "csv"]
     distance_tables = []
 
@@ -82,21 +82,21 @@ if __name__ == "__main__":
 
     #####################
 
-    extracted_type = [["Aux lang TTR", "Overlap word-level", "Overlap subword-level", "Aux lang dataset size", "TTR difference ratio", "Dataset size ratio", "Task lang dataset size"] + distance_category]
+    extracted_type = [["Overlap word-level", "Overlap subword-level", "Transfer lang dataset size", "Target lang dataset size", "Transfer over target size ratio", "Transfer lang TTR", "Target lang TTR", "Transfer target TTR distance"] + distance_category]
 
     for i in range(1, table.shape[0]):
-        lan_task, lan_aux, _, _, _ = table[i]
-        ttr = ttr_table.loc[lan_aux].values[0]
-        overlap_word = overlap_word_table.loc[lan_aux, lan_task] / overlap_word_table.loc[lan_task, lan_task]
-        overlap_subword = overlap_subword_table.loc[lan_aux, lan_task] / overlap_subword_table.loc[lan_task, lan_task]
-        datasize = datasize_table.loc[lan_aux].values[0]
-        ttr_target = ttr_table.loc[lan_task].values[0]
-        ttr_diff = (ttr - ttr_target) / ttr_target
-        datasize_target = datasize_table.loc[lan_task].values[0]
-        datasize_ratio = datasize / datasize_target
-        distance_list = [dtable.loc[lan_aux, lan_task] for dtable in distance_tables]
+        lan_tg, lan_tf, _, _, _ = table[i]
+        overlap_word = overlap_word_table.loc[lan_tf, lan_tg] / overlap_word_table.loc[lan_tg, lan_tg]
+        overlap_subword = overlap_subword_table.loc[lan_tf, lan_tg] / overlap_subword_table.loc[lan_tg, lan_tg]
+        datasize_tf = datasize_table.loc[lan_tf].values[0]
+        datasize_tg = datasize_table.loc[lan_tg].values[0]
+        datasize_tf_to_tg_ratio = datasize_tf / datasize_tg
+        ttr_tf = ttr_table.loc[lan_tf].values[0]
+        ttr_tg = ttr_table.loc[lan_tg].values[0]
+        ttr_distance = (1 - ttr_tf / ttr_tg) ** 2
+        distance_list = [dtable.loc[lan_tf, lan_tg] for dtable in distance_tables]
 
-        extracted_type.append([ttr, overlap_word, overlap_subword, datasize, ttr_diff, datasize_ratio, datasize_target] + distance_list)
+        extracted_type.append([overlap_word, overlap_subword, datasize_tf, datasize_tg, datasize_tf_to_tg_ratio, ttr_tf, ttr_tg, ttr_distance] + distance_list)
 
     table = np.column_stack((table, np.array(extracted_type)))
 
